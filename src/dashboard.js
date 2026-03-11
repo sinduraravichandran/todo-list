6c24c3515aa754f1abaad8459f2b2fdbc5c3d1a7
Sindura Ravichandran <ravichandran.sindura@gmail.com>
2026-03-11 07:31:01 -0400

    adding text

--- src/dashboard.js
+++ src/dashboard.js
@@ -8,11 +8,20 @@ const content = document.getElementById("content");
 export function renderProjects() {
     const projects = getProjects();
     projects.forEach(element => {
+        console.log(element.name)
+        //create div & add class
         const newDiv = document.createElement("div");
         newDiv.classList.add("project");
+
+        //add project details 
+        newDiv.innertext = element.name;
         content.appendChild(newDiv);
-    
+
+        
+
+
     });
 }
 
 
+//figure out why the text is not showing up in the UI on the project
